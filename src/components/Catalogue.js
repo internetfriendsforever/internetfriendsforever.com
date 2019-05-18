57bd6dbe650dbce918757c576638846be93cc031
s-g-k <seb@internetfriendsforever.com>
2019-05-18 16:35:34 +0200

    to columns and colors, type undecided

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -4,7 +4,63 @@ import BlockContent from '@sanity/block-content-to-react'
 import imageUrlBuilder from '../sanity/imageUrlBuilder'
 
 const Figure = styled('figure')`
-  margin: 1em 0;
+  position: relative;
+  margin: 1.5em 0 0 0;
+
+  &:first-child {
+    margin-top: 0;
+  }
+
+  img, video {
+    max-width: 100%;
+  }
+`
+
+const Figcaption = styled('figcaption')`
+  font-size: 1em;
+
+  :hover {
+    background: #c6c6d2;
+    box-shadow: 4px 0px 10px -10px rgba(0,0,0,0.4);
+  }
+
+  h1, h2, h3, p {
+    font-size: 1em;
+    font-weight: normal;
+    margin: 0.5em 0;
+
+    &:first-child {
+      margin-top: 0;
+    }
+  }
+
+  @media (min-width: 680px) {
+    padding: 0.5rem;
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: 40%;
+    max-width: 28rem;
+    border-right: 1px solid rgb(255, 255, 255, 0.3);
+  }
+
+  @media (min-width: 800px) {
+    font-size: 1.4em;
+  }
+`
+
+const Text = styled('div')`
+  position: sticky;
+  top: 0.5rem;
+  left: 0;
+  padding-bottom: 0.5rem;
+  margin-left: 0.5em;
+
+  @media (min-width: 680px) {
+    margin-left: calc(50% + 0.5rem);
+  }
 `
 
 export default function Catalogue ({ catalogue }) {
@@ -12,12 +68,12 @@ export default function Catalogue ({ catalogue }) {
     const { media, description } = item
 
     return (
-      <Figure>
+      <Figure key={item._key}>
         {media.map(item => {
           switch (item._type) {
             case 'image':
               const src = imageUrlBuilder(item)
-                .width(400)
+                .width(1600)
                 .auto('format')
                 .url()
 
@@ -29,7 +85,7 @@ export default function Catalogue ({ catalogue }) {
                 <video
                   key={item._key}
                   src={item.asset.url}
-                  width={400}
+                  width={1600}
                   controls
                 />
               )
@@ -49,9 +105,11 @@ export default function Catalogue ({ catalogue }) {
         })}
 
         {description && (
-          <figcaption>
-            <BlockContent blocks={description} />
-          </figcaption>
+          <Figcaption>
+            <Text>
+              <BlockContent blocks={description} />
+            </Text>
+          </Figcaption>
         )}
       </Figure>
     )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,41 +1,115 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { injectGlobal } from 'emotion'
+import styled from 'react-emotion'
 
 injectGlobal`
   * {
     box-sizing: border-box;
   }
 
+  @font-face {
+    font-family: 'Lyon';
+    font-style: normal;
+    font-weight: 400;
+    src: url('${require('../assets/fonts/LyonText-RegularNo2-Web.woff2')}') format('woff2'),
+      url('${require('../assets/fonts/LyonText-RegularNo2-Web.woff')}') format('woff');
+  }
+
   body {
-    font-family: sans-serif;
-    background: #eee;
+    font-family: Lyon, serif;
+    line-height: 1.1;
+    background: #c6c6d2;
+    margin: 0;
+    color: white;
+
+    @media (min-width: 680px) {
+      font-size: 0.875em;
+      line-height: 1.25;
+    }
+  }
+
+  a {
+    color: inherit;
+    opacity: 0.8;
+    text-decoration: none;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+
+  ::selection {
+    background: white;
+  }
+  ::-moz-selection {
+    background: black;
   }
 `
 
+const Container = styled('div')`
+`
+
+const Header = styled('header')`
+  position: relative;
+  z-index: 10;
+  padding: 0.5em;
+
+  h1, h2, h3, p {
+    font-size: 1em;
+    font-weight: normal;
+    margin: 0.5em 0;
+
+    &:first-child {
+      margin-top: 0;
+    }
+  }
+
+  address {
+    font-style: normal;
+  }
+
+  @media (min-width: 680px) {
+    width: 20%;
+    position: fixed;
+    border-right: 1px solid rgb(255, 255, 255, 0.3);
+    top: 0;
+    left: 0;
+    bottom: 0;
+    max-width: 14rem;
+    // mix-blend-mode: exclusion;
+  }
+`
+
+const Main = styled('main')``
+
 export default function Layout ({ path, user, site, children }) {
   return (
-    <Fragment>
-      <h1>internetfriendsforever</h1>
+    <Container>
+      <Header>
+        <h1>internet<wbr />friends<wbr />forever</h1>
 
-      <address>
-        <p>
-          <a href='http://danielmahal.com/'>Daniel</a> and <a href='http://s-g-k.org/'>Seb</a> working today for our better tomorrow
-        </p>
-        <p>
-          <a title='send us an email' href='mailto:anyone@internetfriendsforever.com'>
-            anyone@internetfriendsforever.com
-          </a>
-        </p>
+        <address>
+          <p>
+            <a href='http://danielmahal.com/'>Daniel</a> and <a href='http://s-g-k.org/'>Seb</a> working today for our better tomorrow
+          </p>
+          <p>
+            <a title='send us an email' href='mailto:anyone@internetfriendsforever.com'>
+              anyone@internet<wbr />friends<wbr />forever.com
+            </a>
+          </p>
 
-        <p>
-          <a title='open map' href='https://www.openstreetmap.org/node/2785466073'>
-            Grønlandsleiret 39<br />
-            0190 Oslo
-          </a>
-        </p>
-      </address>
+          <p>
+            <a title='open map' href='https://www.openstreetmap.org/node/2785466073'>
+              Grønlandsleiret 39<br />
+              0190 Oslo
+            </a>
+          </p>
+        </address>
+      </Header>
 
-      {children}
-    </Fragment>
+      <Main>
+        {children}
+      </Main>
+    </Container>
   )
 }
