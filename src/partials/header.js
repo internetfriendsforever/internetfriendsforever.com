8259df5141fbf7da927ccba0a0b96bccd803597a
Daniel Mahal <danielmahal@gmail.com>
2019-08-27 07:53:30 +0200

    Tweak line breaking in header

--- src/partials/header.js
+++ src/partials/header.js
@@ -18,8 +18,8 @@ const css = {
       display: flex;
 
       section {
-        flex-basis: 20em;
-        margin-right: 1em;
+        flex-basis: 30em;
+        margin-right: 2em;
       }
     }
   `)
@@ -29,21 +29,21 @@ module.exports = () => `
   <header class="${css.header}">
     <section>
       <h1>internetfriendsforever</h1>
-      <p>
-        <a href='http://danielmahal.com/'>Daniel</a> and
-        <a href='http://s-g-k.org/'>Seb</a>
-        working today for your better tomorrow
+      <p class="about">
+        <a href="http://danielmahal.com/">Daniel</a> and
+        <a href="http://s-g-k.org/">Seb</a>
+        working today for your better&nbsp;tomorrow
       </p>
     </section>
 
     <section>
       <p>
-        <a title='Go to map' href='https://www.openstreetmap.org/node/2785466073'>
+        <a title="Go to map" href="https://www.openstreetmap.org/node/2785466073" target="_blank" rel="noopener noreferer">
           Grønlandsleiret 39, 0190 Oslo
         </a>
         <br />
-        <a title='Send us an email' href='mailto:anyone@internetfriendsforever.com'>
-          anyone [at] internetfriendsforever.com
+        <a title="Send us an email" href="mailto:anyone@internetfriendsforever.com">
+          anyone&nbsp;[at]&nbsp;internetfriendsforever.com
         </a>
       </p>
     </section>
