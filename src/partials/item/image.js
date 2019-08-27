111671b7fd817fb54c32d6c47abd9d0854d42091
Daniel Mahal <danielmahal@gmail.com>
2019-08-27 07:54:01 +0200

    Add original image width

--- src/partials/item/image.js
+++ src/partials/item/image.js
@@ -17,7 +17,7 @@ module.exports = item => {
   const max = width
   const range = max - min
   const steps = Math.floor(range / interval)
-  const widths = Array(steps).fill().map((_, i) => min + i * interval)
+  const widths = [...Array(steps).fill().map((_, i) => min + i * interval), width]
   const sources = widths.map(width => sanity.image(item).width(width).auto('format').url())
   const set = sources.map((src, i) => `${src} ${widths[i]}w`)
   const smallest = sources[0]
