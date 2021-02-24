bc35e100e095b4c03ca7364108a090c77866ff51
s-g-k <seb@internetfriendsforever.com>
2021-02-24 09:10:17 +0100

    Fix broken image link, remove XHTML tag endings

--- src/html.js
+++ src/html.js
@@ -10,17 +10,17 @@ module.exports = ({
     <head>
       <title>${title}</title>
       <meta name="description" content="${description}">
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
-      <meta name="theme-color" content="#000" />
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+      <meta name="theme-color" content="#000">
       <link rel="icon" type="image/jpg" href="assets/favicon.png">
-      <meta property="og:description" content="${description}" />
-      <meta property="og:url" content="https://internetfriendsforever.com/" />
-      <meta property="og:image" content="https://internetfriendsforever.com/assets/favicon.jpg" />
-      <meta property="og:image:width" content="1024" />
-      <meta property="og:image:height" content="1024" />
+      <meta property="og:description" content="${description}">
+      <meta property="og:url" content="https://internetfriendsforever.com/">
+      <meta property="og:image" content="https://internetfriendsforever.com/assets/favicon.png">
+      <meta property="og:image:width" content="1024">
+      <meta property="og:image:height" content="1024">
       <script async defer data-domain="internetfriendsforever.com" src="https://plausible.io/js/plausible.js"></script>
-      <link rel="stylesheet" type="text/css" href="styles.css" />
+      <link rel="stylesheet" type="text/css" href="styles.css">
       <style>
         html {
           scroll-behavior: smooth;
